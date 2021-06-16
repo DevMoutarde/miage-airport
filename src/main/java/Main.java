b6af6c2e314b06e35962e54f358bd1c77f257df2
Gabin <fourcault.gabin@gmail.com>
2021-06-16 05:42:54 +0200

    Question3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,24 @@
 public class Main {
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
         Aeroport aeroport = new Aeroport();
 
-        new Avion("gab airline", aeroport).start();
+        Avion gab_airline = new Avion("gab airline", aeroport);
+        Avion ingrid_airline = new Avion("ingrid airline", aeroport);
+
+        gab_airline.start();
+        ingrid_airline.start();
+
+        gab_airline.join();
+        ingrid_airline.join();
+
+        System.out.println("end of threads, rebooting");
+        Thread.sleep(2000);
 
 
+        gab_airline = new Avion("gab airline", aeroport);
+        Avion gab_airline1 = new Avion("gab airline", aeroport);
+        gab_airline.start();
+        gab_airline1.start();
     }
 }
